cc2b0097b68dd7615cd9c2b8674d03eb6a17a3dd
Nuhcan <nuhcanatar20@gmail.com>
2024-08-10 22:18:55 +0300

    Step Second

--- src/Controller/AppViewGeneralController.php
+++ src/Controller/AppViewGeneralController.php
@@ -9,19 +9,32 @@ use Symfony\Component\Routing\Annotation\Route;
 class AppViewGeneralController extends AbstractController
 {
     private array $viewList = [
-       ['title' => 'Hello', 'created' => '2024/01/10',],
-       ['title' => 'Bye', 'created' => '2022/05/12',],
+       ['title' => 'Flutter', 'created' => '2024/01/10',],
+       ['title' => 'Php', 'created' => '2022/05/12',],
+       ['title' => 'Dart', 'created' => '2022/05/12',],
+       ['title' => 'Firebase', 'created' => '2022/05/12',],
+       ['title' => 'Docker', 'created' => '2022/05/12',],
     ];
-
+    
     #[Route('/{index<\d+>?3}', name: 'app_index')]
-    public function initializeApp(int $index = 3): Response
+    public function initializeApp(int $index = null): Response
     {
+        // Eğer index sağlanmamışsa veya index geçersizse, tüm listeyi göster
+        if ($index === null || $index < 0) {
+            $index = count($this->viewList); // veya bir varsayılan değer
+        } else {
+            $viewListLength = count($this->viewList);
+            $index = min($index, $viewListLength);
+        }
+
+        $viewListSlice = array_slice($this->viewList, 0, $index);
+
         return $this->render('view/main_page.html.twig', [
-            'viewList' => $this->viewList,
+            'viewList' => $viewListSlice,
             'index' => $index,
         ]);
     }
-    
+
     #[Route('/viewList/{id<\d+>}', name: 'first_direct')]
     public function showFirstDirect(int $id): Response
     {
@@ -38,7 +51,9 @@ class AppViewGeneralController extends AbstractController
     public function showSecondDirect(int $id): Response
     {
         if (!isset($this->viewList[$id])) {
-            throw $this->createNotFoundException('Message not found');
+            throw $this->render('view/detail/detail_view.html.twig', [
+            
+            ]);
         }
     
         return $this->render('view/detail/detail_view.html.twig', [

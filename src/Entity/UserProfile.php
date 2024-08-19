5a1c49598b1e849ed432aad0b723bdad7a2480e5
Nuhcan <nuhcanatar20@gmail.com>
2024-08-19 14:06:54 +0300

    Symfony Course last lesson commitment.

--- src/Entity/UserProfile.php
+++ src/Entity/UserProfile.php
@@ -39,6 +39,9 @@ class UserProfile
     #[ORM\JoinColumn(nullable: false)]
     private ?User $user = null;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $image = null;
+
 
 
     public function getId(): ?int
@@ -138,6 +141,18 @@ class UserProfile
     public function setUser(User $user): static
     {
         $this->user = $user;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
 
         return $this;
     }
